3d345a8d91163482c93cbe19216d93660e5f520d
Алексей Кожемякин <aleksey.k2000@mail.ru>
2019-08-04 14:20:45 +0700

    Add work with exceptions

--- src/Converter.java
+++ src/Converter.java
@@ -2,60 +2,69 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Converter {
+    public static boolean wrong(int vr){
+        if(vr < 1 || vr > 36){
+            return true;
+        }
+        else return false;
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String[] st = new String[2];
         double workingC;
-        System.out.print("Введите исходную систему счисления: ");
-        int now = scan.nextInt();
-        System.out.print("Введите конвертируемое число: ");
-        String start = scan.next();
-        if(start.contains(".")){
-            st = start.split(Pattern.quote("."));
-        }
-        System.out.print("Введите систему счисления результата: ");
-        int result = scan.nextInt();
-        if(now == 1){
-            workingC = start.length();
-        }
-        else{
-            if(start.contains(".")){
-                workingC = Integer.parseInt(st[0], now);
-                for(int i = 0; i < st[1].length(); i++){
-                    if(Character.isDigit(st[1].charAt(i))) {
-                        workingC += (st[1].charAt(i) - '0') / Math.pow(now, i + 1);
+        try {
+            System.out.print("Введите исходную систему счисления: ");
+            int now = scan.nextInt();
+            System.out.print("Введите конвертируемое число: ");
+            String start = scan.next();
+            if (start.contains(".")) {
+                st = start.split(Pattern.quote("."));
+            }
+            System.out.print("Введите систему счисления результата: ");
+            int result = scan.nextInt();
+            if (wrong(result) || wrong(now)) {
+                System.out.println("error");
+                return;
+            }
+            if (now == 1) {
+                workingC = start.length();
+            } else {
+                if (start.contains(".")) {
+                    workingC = Integer.parseInt(st[0], now);
+                    for (int i = 0; i < st[1].length(); i++) {
+                        if (Character.isDigit(st[1].charAt(i))) {
+                            workingC += (st[1].charAt(i) - '0') / Math.pow(now, i + 1);
+                        } else {
+                            workingC += (st[1].charAt(i) - 'a' + 10) / Math.pow(now, i + 1);
+                        }
                     }
-                    else {
-                        workingC += (st[1].charAt(i) - 'a' + 10) / Math.pow(now, i + 1);
-                    }
+                } else {
+                    workingC = Integer.parseInt(start, now);
                 }
             }
-            else{
-                workingC = Integer.parseInt(start, now);
-            }
-        }
-        if(result == 1){
-            for(int i = 0; i < Math.floor(workingC); i++){
-                System.out.print("1");
-            }
-        }
-        else{
-            System.out.print(Integer.toString((int)workingC, result));
-            if(workingC != Math.floor(workingC)){
-                workingC -= Math.floor(workingC);
-                System.out.print(".");
-                for(int i = 0, check; workingC != 0 && i < 5; i++){
-                    workingC *= result;
-                    check = (int)workingC;
-                    if(check < 10) {
-                        System.out.print(check);
-                    }
-                    else{
-                        System.out.print((char)('a' + check - 10));
-                    }
+            if (result == 1) {
+                for (int i = 0; i < Math.floor(workingC); i++) {
+                    System.out.print("1");
+                }
+            } else {
+                System.out.print(Integer.toString((int) workingC, result));
+                if (workingC != Math.floor(workingC)) {
                     workingC -= Math.floor(workingC);
+                    System.out.print(".");
+                    for (int i = 0, check; workingC != 0 && i < 5; i++) {
+                        workingC *= result;
+                        check = (int) workingC;
+                        if (check < 10) {
+                            System.out.print(check);
+                        } else {
+                            System.out.print((char) ('a' + check - 10));
+                        }
+                        workingC -= Math.floor(workingC);
+                    }
                 }
             }
+        } catch(Exception e){
+        System.out.println("error");
         }
     }
 }
